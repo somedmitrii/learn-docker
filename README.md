56bd2c6b238a0e2b2b1904de6638c04d579362d5
somedmitrii <somedmitrii@gmail.com>
2022-10-23 17:59:44 +0400

    volumes

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # learn-docker
 
 https://docs.docker.com/compose/compose-file/
+
+
+docker volume - managed volumes(хранилища)
+
+docker-compose -f first.yml -f second.yml up --build конфиг second расширяет first
--- api/src/index.js
+++ api/src/index.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const mongoose = require("mongoose");
+
 const { connectDb } = require("./db");
 const { port, host, db } = require("./config");
 
@@ -8,11 +10,20 @@ app.get("/test", (req, res) => {
   res.send("Api test route");
 });
 
+const postSchema = new mongoose.Schema({ name: String });
+const Post = mongoose.model("Post", postSchema);
+
 const startServer = () => {
   app.listen(port, () => {
     console.log(`listen port on ${port} port`);
     console.log(`on host ${host}`);
     console.log(`on database ${db}`);
+
+    const silence = new Post({ name: "Silence" });
+    silence.save((err, savedSilence) => {
+      if (err) return console.log(err);
+      console.log("savedSilence with", savedSilence);
+    });
   });
 };
 
