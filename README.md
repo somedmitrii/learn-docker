ae1474a396e5d14f972e53fba1a915f155e43241
somedmitrii <somedmitrii@gmail.com>
2022-10-23 17:32:55 +0400

    add mongo db connection

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # learn-docker
+
+https://docs.docker.com/compose/compose-file/
---
+++ api/src/index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const { connectDb } = require("./db");
+const { port, host, db } = require("./config");
+
+const app = express();
+
+app.get("/test", (req, res) => {
+  res.send("Api test route");
+});
+
+const startServer = () => {
+  app.listen(port, () => {
+    console.log(`listen port on ${port} port`);
+    console.log(`on host ${host}`);
+    console.log(`on database ${db}`);
+  });
+};
+
+connectDb()
+  .on("error", console.log)
+  .on("disconnected", connectDb)
+  .once("open", startServer);
